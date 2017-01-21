3d8edbffc17f6df23a485b6ebbe53b57b62aad0c
Sophia Quach <sophiaquach16@Sophias-MacBook-Air.local>
2017-01-21 18:20:46 -0500

    separated into components, fixed input problem

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,28 @@
 <template>
-  <div class="inputForm">
-      <form novalidate @submit.stop.prevent="submit">
+  <div>
+    <input-form></input-form>
 
-        <md-input-container>
-          <label>Event Name</label>
-          <md-input v-model="eventName" placeholder="Event Name"></md-input>
-        </md-input-container>
 
-        <md-input-container>
-          <label>ddmmyyyy format</label>
-          <md-input v-model="date" placeholder="Date"></md-input>
-        </md-input-container>
 
-        <md-input-container>
-          <label>24 hour format, ex.22:00 for 10pm</label>
-          <md-input v-model="startTime" placeholder="Start Time"></md-input>
-        </md-input-container>
+  <md-list class="custom-list md-triple-line">
+    <md-list-item>
+    <md-avatar>
+      <img src="https://placeimg.com/40/40/people/1" alt="People">
+    </md-avatar>
 
-        <md-input-container>
-          <label>24 hour format, ex.22:00 for 11pm</label>
-          <md-input v-model="endTime" placeholder="End Time"></md-input>
-        </md-input-container>
 
-        <md-input-container>
-          <label>Duration between each check-up (in minutes)</label>
-          <md-input v-model="durationInBetween" type="number"></md-input>
-        </md-input-container>
 
-        <md-button class="md-raised md-primary">Record Message</md-button>
-
-        <md-button class="md-fab md-fab-bottom-left">+</md-button>
-      </form>
 </div>
 </template>
 
 <script>
+  import InputForm from './components/InputForm'
+  export default {
+    name: 'app',
+    components: {
+      InputForm
+    }
+  }
 </script>
 <style>
 
---
+++ src/components/eventList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="inputForm">
+    <div class="md-list-text-container">
+      <p>
+        {{ newEventName }}
+      </br>
+        {{ newDate }}
+      </br>
+        {{ newStartTime }} -- {{ newEndTime }}
+      </br>
+      </p>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'eventList',
+  props: [
+    'thing',
+    'newEventName',
+    'newDate',
+    'newStartTime',
+    'newEndTime',
+    'newDuration'
+  ]
+}
+</script>
+<style>
+
+  .inputForm {
+    padding: 20px;
+  }
+
+</style>
